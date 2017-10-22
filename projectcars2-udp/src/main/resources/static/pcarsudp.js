8de2e844e7a044b17d7720501138d4d513590efc
laurent <laurent@XPS-L702X>
2017-10-22 16:29:12 -0400

    Move src

--- projectcars2-udp/src/main/resources/static/pcarsudp.js
+++
@@ -1,9 +0,0 @@
-function loaddata(){
-	var url = window.location;
-    $('#data').load(url+'/refresh');
-}
-
-
-setInterval(function(){
-    loaddata() // this will run after every 5 seconds
-}, 5000)
